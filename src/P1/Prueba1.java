3512a09837372616d1bbc996291403b99a798b8d
Your Name <you@example.com>
2024-11-18 15:36:16 +0100

    Update Prueba1.java

--- src/P1/Prueba1.java
+++ src/P1/Prueba1.java
@@ -4,5 +4,12 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Holaa Cabronesssss   No la cagues mas");
 	}
+	int[] sumArray = {4,36,4,23,867};
+	int[] subArray = {785675, 56, 5675, 56544, 234};
+	int[] prodArray = {6, 8, 20, 23, 12};
+	
+	// User 3 fer la multiplicació dels elements de prodArray
+	
+	
 
 }
