24f2b100aaa11f61b85a9c407b3b28262a51203f
Your Name <you@example.com>
2024-11-18 16:35:45 +0100

    Update Prueba1.java

--- src/P1/Prueba1.java
+++ src/P1/Prueba1.java
@@ -32,13 +32,11 @@ public class Prueba1 {
 				// User 3 fer la multiplicació dels elements de prodArray
 				 
 				 
-	}
-	int[] sumArray = {4,36,4,23,867};
-	int[] subArray = {785675, 56, 5675, 56544, 234};
-	int[] prodArray = {6, 8, 20, 23, 12};
+	
 	
 	// User 3 fer la multiplicació dels elements de prodArray
 	
 	
 
+}
 }
